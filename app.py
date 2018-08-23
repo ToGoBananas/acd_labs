44c3498ccdcabea4024932798247a7246e163594
Andrei Khaliaukin <endpoo@gmail.com>
2018-08-23 18:25:50 +0300

    add error handling

--- app.py
+++ app.py
@@ -7,8 +7,11 @@ from pandas import read_csv
 @hug.post('/csv/sort')
 def csv_sort(content: hug.types.text):
     """Sorts data from content attribute and returns JSON to user."""
-
-    data_frame = read_csv(io.StringIO(content), sep='\t', header=None)
-    data_frame = data_frame.sort_values(by=list(data_frame.columns.values))
-    csv_data = data_frame.to_csv(sep='\t', header=None, index=False)
-    return {'result': csv_data}
+    try:
+        data_frame = read_csv(io.StringIO(content), sep='\t', header=None)
+    except Exception:
+        return {'error': 'Invalid data.'}
+    else:
+        data_frame = data_frame.sort_values(by=list(data_frame.columns.values))
+        csv_data = data_frame.to_csv(sep='\t', header=None, index=False)
+        return {'result': csv_data}
